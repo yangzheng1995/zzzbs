bcb8685264c3bfb589751d3270c037594d3fe19c
Yang Zheng <875333005@qq.com>
2018-04-17 01:45:12 +0800

    Finish user edit, update, index, and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
   	if user && user.authenticate(params[:session][:password])
   	  log_in user
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      remember user
+      redirect_back_or user
     #  redirect_to user
   	else
   	  flash.now[:danger] = '无效的电子邮件/密码组合'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+    before_action :correct_user, only: [:edit, :update]
+    before_action :admin_user, only: :destroy
+
+  def index 
+   @users = User.paginate(page: params[:page])
+  end
 
   def show
   	@user = User.find(params[:id])
@@ -19,13 +26,58 @@ class UsersController < ApplicationController
   	end
   end
 
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+       flash[:success] = "头像更新"
+       redirect_to @user
+    else
+      render 'edit' 
+    end
+  end
+
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+
   private
 
-
+  #事前过滤器
   def user_params
     params.require(:user).permit(:name, :email, :password,
                                          :password_confirmation)
   end
 
+  #确保用户已经登录
+  def logged_in_user
+    unless logged_in?
+      store_location
+    flash[:danger] = "Please log in." 
+    redirect_to login_url
+    end
+  end
+
+
+  #确保是正确用户
+  def correct_user
+    @user = User.find(params[:id])
+     redirect_to(root_url) unless current_user?(@user) 
+  end
+
+
+  #确保是管理员
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
 
 end
+
