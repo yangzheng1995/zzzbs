bb122c6e2c91b39f704c7dac4176c2c4a34cd3f7
Yang Zheng <875333005@qq.com>
2018-04-19 17:57:46 +0800

    12

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,7 @@ class StaticPagesController < ApplicationController
   	#@micropost = current_user.microposts.build if logged_in?
   	if logged_in?
   	  @micropost  = current_user.microposts.build
-  	  @feed_items = current_user.feed.paginate(page: params[:page])
+     @feed_items = current_user.feed.paginate(page: params[:page])
     end
   end
 
