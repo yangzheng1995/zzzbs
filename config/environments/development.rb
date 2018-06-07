94d242a0da03a82d9e1355c2213d4de6f300f132
Yang Zheng <875333005@qq.com>
2018-06-07 10:36:04 +0800

    fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,19 +22,39 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: host }
 
 
-  ActionMailer::Base.delivery_method = :smtp  
-  config.action_mailer.perform_deliveries = true  
-  config.action_mailer.raise_delivery_errors = true  
-  config.action_mailer.default :charset => "utf-8"  
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }  
-  ActionMailer::Base.smtp_settings = {    
-    :address => "smtp.163.com",  
-    :port => 25,  
-    :domain => "163.com",  
-    :authentication => :login,  
-    :user_name => "tyzz163@163.com",  
-    :password => "yz875333005"  
-  }  
+  # ActionMailer::Base.delivery_method = :smtp  
+  # config.action_mailer.perform_deliveries = true  
+  # config.action_mailer.raise_delivery_errors = true  
+  # config.action_mailer.default :charset => "utf-8"  
+  # config.action_mailer.default_url_options = { :host => 'localhost:3000' }  
+  # ActionMailer::Base.smtp_settings = {    
+  #   :address => "smtp.163.com",  
+  #   :port => 25,  
+  #   :domain => "163.com",  
+  #   :authentication => :login,  
+  #   :user_name => "tyzz163@163.com",  
+  #   :password => "yz875333005"  
+  # }  
+
+
+Rails.application.configure do
+    #...只是为了演示邮箱配置的位置，所以省略了其余的配置...
+    ActionMailer::Base.delivery_method = :smtp  
+    config.action_mailer.perform_deliveries = true  
+    config.action_mailer.raise_delivery_errors = true  
+    config.action_mailer.default :charset => "utf-8"  
+    ActionMailer::Base.smtp_settings = {    
+        :address => "smtp.163.com", #邮件服务器地址 
+        :port => 25,  
+        :domain => "163.com", #服务器域名，如xxx@yeah.net域名就是yeah.net
+        :authentication => :login,   
+        :user_name => "tyzz163@163.com",  #邮件用户名，如xxx@yeah.net用户名就是xxx
+        :password => "yz875333005",  #与登录密码不同，此处是客户端授权密码，切记！
+  }
+end
+
+
+
 
 
   # Print deprecation notices to the Rails logger.
